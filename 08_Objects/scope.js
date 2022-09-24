97ebffa71fa80cf69e1edf4b158d50c651f8b3f4
Ahmed <ahmedpanjwani.in35@gmail.com>
2022-09-24 14:52:24 +0530

    Done with the Objects Exercise : Day 08

--- 08_Objects/scope.js
+++ 08_Objects/scope.js
@@ -1,11 +1,11 @@
-a = "JavaScript"
-b = 10
+// a = "JavaScript"
+// b = 10
 
-function letLearnScope(){
-    console.log(a, b)
-    if(true){
-        console.log(a, b)
-    }
-}
+// function letLearnScope(){
+//     console.log(a, b)
+//     if(true){
+//         console.log(a, b)
+//     }
+// }
 
-console.log(a, b)
+// console.log(a, b)
